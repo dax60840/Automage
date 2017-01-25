982d99b75efae7f4269402bc7015e4b99827fc6a
FeNo <csfeno@gmail.com>
2017-01-25 11:16:11 +0100

    Art
    art

---
+++ Assets/RandomMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterial : MonoBehaviour
+{
+
+	public Material[] materials;
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<MeshRenderer> ().material = materials [Random.Range (0, materials.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
